8b306e27e5712918e1e05ec458c972456baf77ad
David Sklar <sklard@tumblr.com>
2016-06-24 21:44:11 +0000

    Cloud9 conveniences for setup and for launching the server in different ways/modes.

---
+++ tools/c9-setup.sh
@@ -0,0 +1,28 @@
+echo "Starting the postgresql service..."
+sudo service postgresql start
+
+echo "Creating the midas database..."
+createdb midas
+psql midas <<EOF
+CREATE USER midas WITH PASSWORD 'midas';
+GRANT ALL PRIVILEGES ON DATABASE midas to midas;
+ALTER SCHEMA public OWNER TO midas;
+EOF
+
+echo "npm install -g node-gyp grunt-cli"
+npm install -g node-gyp grunt-cli
+
+echo "npm install"
+npm install
+
+echo "Symlinking to the globally-installed grunt..."
+pushd node_modules/.bin
+ln -s `which grunt` .
+popd
+
+echo "Doing initial schema deployment..."
+sh tools/postgres/cleandb.sh
+
+echo "Registering sample tasks and launching web server..."
+npm run demo
+
