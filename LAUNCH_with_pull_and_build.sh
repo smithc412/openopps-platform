b9b12c06b3e5f0945d1a3f3e1761f801af63ccf5
vaopenopps <dfsklar@gmail.com>
2016-06-26 19:31:07 +0000

    improvements on the c9 scripts

--- LAUNCH_with_pull_and_build.sh
+++ LAUNCH_with_pull_and_build.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
+echo "Starting the postgresql service..."
+sudo service postgresql start
+
+echo "Fetching latest version and forcing compilation..."
 git pull
 npm run build
 
 # Kill any currently-running instance of the server
-killall node
+killall -q node
 # ... then launch new instance of the server
 node app.js
