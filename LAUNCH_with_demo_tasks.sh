bd6c3b75ae435c5287384048d6c8fa8bb7757100
dfsklar <dfsklar@gmail.com>
2016-06-26 15:08:09 -0400

    Update LAUNCH_with_demo_tasks.sh

--- LAUNCH_with_demo_tasks.sh
+++ LAUNCH_with_demo_tasks.sh
@@ -2,5 +2,6 @@
 
 # Kill any currently-running instance of the server
 killall node
-# ... then launch new instance of the server
+# ... then launch new instance of the server with fresh DB
+sh tools/postgres/cleandb.sh
 npm run demo
